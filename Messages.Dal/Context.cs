fb2361b1c0fdddcd4ad333fc1dfc146a17f905bc
genady <puhovkostya1@gmail.com>
2025-03-29 22:35:07 +0300

    update compose file

--- Messages.Dal/Context.cs
+++ Messages.Dal/Context.cs
@@ -6,7 +6,9 @@ namespace Messages.Dal;
 public class Context : DbContext
 {
     public Context(DbContextOptions<Context>? options) : base(options)
-    { }
+    {
+        Database.EnsureCreated();
+    }
     
     public DbSet<UserEntity> Users { get; set; }
 
